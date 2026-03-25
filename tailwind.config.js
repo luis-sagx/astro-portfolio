ed68002641857b679758da4c9d4c2603d04e286e
Luis Sagnay <sagnayluis99@gmail.com>
2026-03-24 22:24:11 -0500

    feat: implement responsive navbar with theme toggle and GSAP animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{html,ts}",
+        "./src/**/*.{astro,html,ts}",
     ],
     darkMode: 'class',
     theme: {
