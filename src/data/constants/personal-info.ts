bab7e0f636dac80443cda21d976351fd59be0140
Luis Sagnay <sagnayluis99@gmail.com>
2026-03-25 21:27:07 -0500

    feat: add hero section components and personal info constants
    - Created HeroActions component for CV download and contact link.
    - Added HeroBackground component for decorative background elements.
    - Implemented HeroProfileImage component to display user profile image.
    - Developed HeroSocialLinks component for social media links.
    - Introduced personal-info constants for user details and resume URL.

---
+++ src/data/constants/personal-info.ts
@@ -0,0 +1,15 @@
+import type { PersonalInfo } from '../models/portfolio.model'
+
+export const PERSONAL_INFO: PersonalInfo = {
+  firstname: 'Luis',
+  lastname: 'Sagnay',
+  profession: 'Software Engineer Student',
+  interests: ['Development', 'AI', 'Cybersecurity'],
+  bio: [
+    'Software Engineering student at',
+    'Universidad de las Fuerzas Armadas ESPE.',
+    'I am constantly seeking to learn new technologies and take on challenges that help me grow professionally.',
+  ],
+  imageUrl: 'LuisS.png',
+  resumeUrl: 'assets/resume/Luis_Sagnay_CV.pdf',
+}
