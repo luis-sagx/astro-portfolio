d416b943a9d714754a48fd98a080354ff760eb7c
Luis Sagnay <sagnayluis99@gmail.com>
2026-04-01 20:52:42 -0500

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: 'https://luissagnay.dev',
+  integrations: [icon(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
